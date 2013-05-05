aaa41417ade521d10c79f08bd00e15d1b08461f3
JonnyD <contact@jonnydevine.com>
2013-05-05 15:43:57 +0100

    Check if group already exists

--- src/groups/command/commands/CreateGroupCommand.java
+++ src/groups/command/commands/CreateGroupCommand.java
@@ -4,6 +4,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import groups.command.PlayerCommand;
+import groups.model.Group;
 
 public class CreateGroupCommand extends PlayerCommand {
 
@@ -20,17 +21,26 @@ public class CreateGroupCommand extends PlayerCommand {
 		if(!(sender instanceof Player)) {
 			return true;
 		}
+		
 		String name = args[0];
 		
 		int minNameLength = 3;
 		int maxNameLength = 16;
-		boolean greaterThanEqualMin = name.length() > minNameLength;
+		boolean greaterThanEqualMin = name.length() >= minNameLength;
 		boolean lessThanEqualMax = name.length() <= maxNameLength;
 		if(!greaterThanEqualMin || !lessThanEqualMax) {
 			sender.sendMessage("Name can't be less than " + minNameLength +
-					" characters and greater than " + maxNameLength + 
+					" characters or greater than " + maxNameLength + 
 					" characters");
+			return true;
 		}
+		
+		Group group = groupManager.getGroupByName(name);
+		if(group != null) {
+			sender.sendMessage("Group already exists");
+			return true;
+		}
+		
 		groupManager.createGroup(name, sender.getName());
 		return true;
 	}
