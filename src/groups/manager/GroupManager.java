a99666fa93e5234cfc7971210330704a8495453c
JonnyD <contact@jonnydevine.com>
2013-05-05 05:25:52 +0100

    Added createPersonalGroup

--- src/groups/manager/GroupManager.java
+++ src/groups/manager/GroupManager.java
@@ -14,11 +14,20 @@ public class GroupManager {
 	}
 	
 	public void createGroup(String name, String username) {
+		createGroup(name, username, false);
+	}
+	
+	public void createGroup(String name, String username, Boolean isPersonal) {
 		Group group = new Group(name);
+		group.setPersonal(isPersonal);
 		Member member = new Member(group, username);
 		member.setRole("admin");
 		group.addMember(member);
 		saveGroup(group);
+	}
+	
+	public void createPersonalGroup(String name, String username) {
+		createGroup(name, username, true);
 	}
 	
 	public void saveGroup(Group group) {
