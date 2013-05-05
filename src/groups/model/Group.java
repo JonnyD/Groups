b7ebae87e7579379cf277c0e026963c31afe53fb
JonnyD <contact@jonnydevine.com>
2013-05-05 22:43:22 +0100

    Generated hashCode() and equals()

--- src/groups/model/Group.java
+++ src/groups/model/Group.java
@@ -88,10 +88,6 @@ public class Group {
 	public void setPersonal(Boolean personal) {
 		this.personal = personal;
 	}
-
-	public void addMember(Member member) {
-		members.add(member);
-	}
 	
 	public Timestamp getUpdatetime() {
 		return updatetime;
@@ -117,12 +113,46 @@ public class Group {
 		this.type = type;
 	}
 
+	public void addMember(Member member) {
+		members.add(member);
+	}
+	
+	public void removeMember(Member member) {
+		members.remove(member);
+	}
+
 	public List<Member> getMembers() {
 		return members;
 	}
 	
 	public void setMembers(List<Member> members) {
 		this.members = members;
+	}
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Group other = (Group) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 
 	@Override
--- src/groups/model/Member.java
+++ src/groups/model/Member.java
@@ -115,6 +115,38 @@ public class Member {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((group == null) ? 0 : group.hashCode());
+		result = prime * result
+				+ ((playerName == null) ? 0 : playerName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Member other = (Member) obj;
+		if (group == null) {
+			if (other.group != null)
+				return false;
+		} else if (!group.equals(other.group))
+			return false;
+		if (playerName == null) {
+			if (other.playerName != null)
+				return false;
+		} else if (!playerName.equals(other.playerName))
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return "Member [id=" + id + ", group=" + group + ", playerName="
 				+ playerName + ", role=" + role + ", createTime=" + createTime
