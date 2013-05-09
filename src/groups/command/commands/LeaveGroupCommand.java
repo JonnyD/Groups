4e5ea8970f53e97334995c84d66af2f7c213c160
JonnyD <contact@jonnydevine.com>
2013-05-09 22:33:00 +0100

    Can't leave group if you are the only admin

--- src/groups/command/commands/LeaveGroupCommand.java
+++ src/groups/command/commands/LeaveGroupCommand.java
@@ -1,5 +1,8 @@
 package groups.command.commands;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -33,10 +36,32 @@ public class LeaveGroupCommand extends PlayerCommand {
 			return true;
 		}
 		
+		if(group.getPersonal()) {
+			sender.sendMessage("You can't leave a Personal Group");
+			return true;
+		}
+		
 		GroupMember groupMember = group.getGroupMember(username);
 		if(groupMember == null || groupMember.getRole() == Role.BANNED) {
 			sender.sendMessage("You are not a member of this group");
 			return true;
+		}
+		
+		Collection<GroupMember> groupMembers = group.getGroupMembers().values();
+		if(groupMember.getRole() == Role.ADMIN) {
+			int countAdmins = 0;
+			
+			for(GroupMember gm : groupMembers) {
+				if(gm.getRole() == Role.ADMIN) {
+					countAdmins++;
+				}
+			}
+			
+			if(countAdmins < 2) {
+				sender.sendMessage("You are the last admin so you can't leave just yet. " +
+						"Either delete the group entirely or assign someone else as an admin.");
+				return true;
+			}
 		}
 		
 		group.removeGroupMemmber(groupMember);
